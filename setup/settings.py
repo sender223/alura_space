e1fd7d2c5acf7ceef769c6f9d835068470c3b2af
Sender SS. Santos <sender.santos@zion.local>
2023-08-03 14:35:50 -0300

    projeto alura space persistencia de dados e admin

--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
+    'galeria.apps.GaleriaConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +126,11 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#QUESTÕES DE MIDIA
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
